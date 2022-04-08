9798190403d7f137b6ffba2a01bc8da9be50d7fb
mo3az <mo3az.abdellatif@gmail.com>
2022-04-09 01:17:26 +0200

    mobile

--- js/main.js
+++ js/main.js
@@ -3,22 +3,37 @@ $(window).on("load", function () {
   lazyLoad();
 });
 $(document).ready(function () {
-  $(".main-item").hover(
-    function () {
-      $(this).width("45%");
-      $(this).children().get(0).play();
-    },
-    function () {
-      $(this).width("calc(100% / 3)");
-      $(this).children().get(0).pause();
-    }
-  );
-
+  if ($(window).width() > 1199) {
+    $(".main-item").hover(
+      function () {
+        $(this).width("45%");
+        $(this).children().get(0).play();
+      },
+      function () {
+        $(this).width("calc(100% / 3)");
+        $(this).children().get(0).pause();
+      }
+    );
+  } else {
+    $(".main-item").click(function () {
+      $(".main-item").not(this).removeClass("active");
+      $(this).toggleClass("active");
+      if ($(this).children(".text").css("display") == "none") {
+        $(this).children(".text").slideDown(500);
+        $(this).children().get(0).play();
+      } else {
+        $(this).children(".text").slideUp(500);
+        $(this).children().get(0).pause();
+      }
+      $(".main-item").not(this).children(".text").slideUp(500);
+      $(".main-item").not(this).children().get(0).pause();
+    });
+  }
   var partners = new Swiper(".partners-slider .swiper", {
     loop: true,
     breakpoints: {
       0: {
-        slidesPerView: 2,
+        slidesPerView: 3,
         spaceBetween: 8,
       },
       768: {
@@ -48,7 +63,7 @@ $(document).ready(function () {
     loop: true,
     breakpoints: {
       0: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 8,
       },
       768: {
@@ -83,19 +98,19 @@ $(document).ready(function () {
     breakpoints: {
       0: {
         slidesPerView: 1,
-        spaceBetween: 30,
+        spaceBetween: 0,
       },
       992: {
         slidesPerView: 2,
-        spaceBetween: 15,
+        spaceBetween: 0,
       },
       1200: {
         slidesPerView: 3,
-        spaceBetween: 15,
+        spaceBetween: 0,
       },
       1700: {
         slidesPerView: 3,
-        spaceBetween: 28,
+        spaceBetween: 8,
       },
     },
     pagination: {
@@ -104,7 +119,7 @@ $(document).ready(function () {
     },
   });
 
-  var news = new Swiper(".testimonials-slider .swiper", {
+  var testimonials = new Swiper(".testimonials-slider .swiper", {
     loop: true,
     breakpoints: {
       0: {
@@ -129,4 +144,33 @@ $(document).ready(function () {
       clickable: true,
     },
   });
+
+  ///////// **footer** /////////
+  if ($(window).width() <= 1199) {
+    $(".footer-list-title").addClass("mo-accordion");
+    $(".footer-list").addClass("mo-panel");
+  }
+  ///////// **ACC** /////////
+  $(".mo-accordion").click(function () {
+    $(".mo-accordion").not(this).removeClass("active");
+    $(this).toggleClass("active");
+    if ($(this).siblings().css("display") == "none") {
+      $(this).siblings().slideDown(500);
+    } else {
+      $(this).siblings().slideUp(500);
+    }
+    $(".mo-accordion").not(this).siblings().slideUp(500);
+  });
+
+   ///////// **menu** /////////
+   if ($(window).width() <= 1199) {
+    $('.menu-btn').click(function () {
+        $(".header-nav").slideDown();
+        $("body").addClass("overflow");
+    });
+    $('.close-btn').click(function () {
+        $(".header-nav").slideUp();
+        $("body").removeClass("overflow");
+    });
+}
 });
